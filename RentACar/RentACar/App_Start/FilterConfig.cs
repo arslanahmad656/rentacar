36555a0c52ab2e052758e73c2976e12ccfe2ff33
Arslan Ahmad 656 <arslanahmad656@live.com.pk>
2018-03-29 20:01:54 +0500

    Filter removed in app_start FilterConfig file

--- RentACar/RentACar/App_Start/FilterConfig.cs
+++ RentACar/RentACar/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace RentACar
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
